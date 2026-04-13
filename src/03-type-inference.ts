47187f64670909815e80065bac84a2560ecd2741
Yashraj_K <yashrajkamble7727@gmail.com>
2026-04-13 16:53:10 +0530

    Type Inference

---
+++ src/03-type-inference.ts
@@ -0,0 +1,5 @@
+let inferenceString = "this is a string"
+
+inferenceString= 1
+
+// Type inference in TypeScript is the compiler's ability to automatically determine a variable's data type based on its initialization and usage, eliminating the need for manual type annotations. This feature ensures type safety while reducing code verbosity.
